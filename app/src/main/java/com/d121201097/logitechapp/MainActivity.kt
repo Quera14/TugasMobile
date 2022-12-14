467378ff4b5cf6fca8e0aa44e00f60bd1531b428
Ali Husain <aliihusainn028@gmail.com>
2022-12-14 23:50:31 +0800

    initial commit

---
+++ app/src/main/java/com/d121201097/logitechapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.d121201097.logitechapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.d121201097.logitechapp.R
+import com.d121201097.logitechapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(TodoListFragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.todoListNav -> replaceFragment(TodoListFragment())
+                R.id.todoHistoryNav -> replaceFragment(TodoHistoryFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainFrameLayout,fragment)
+        fragmentTransaction.commit()
+    }
+}
